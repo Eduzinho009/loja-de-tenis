11803088bc44a7550d981e14dce02d5f4771b6d9
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-03-20 15:44:31 -0300

    Login atualizado e finalizado!

--- Login/script.js
+++ Login/script.js
@@ -2,19 +2,16 @@
 function login() {
     var nome = document.getElementById("nome").value;
     var senha = document.getElementById("senha").value;
-
     if (nome === "admin" && senha === "1234") {
         alert("Login bem-sucedido!");
     } else {
         document.getElementById("error-modal").style.display = "block";
     }
 }
-
 // 2 Lembrete - Função para fechar o modal de erro
 function fecharError() {
     document.getElementById("error-modal").style.display = "none";
 }
-
 // 3 Lembrete - Função para mostrar ou ocultar a senha
 function showPassword() {
     var senhaInput = document.getElementById("senha");
@@ -25,3 +22,4 @@ function showPassword() {
         senhaInput.type = "password";
     }
 }
+
