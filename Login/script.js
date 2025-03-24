3aaf28d02c2c2f7792828c8bd606c0e3e610ed7c
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-03-24 14:41:53 -0300

    Login atualizado e finalizadogit add .

--- Login/script.js
+++ Login/script.js
@@ -2,39 +2,16 @@
 function login() {
     var nome = document.getElementById("nome").value;
     var senha = document.getElementById("senha").value;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
->>>>>>> dev
     if (nome === "admin" && senha === "1234") {
         alert("Login bem-sucedido!");
-=======
-    if (nome === "admin" && senha === "1234") {
-        window.location.href = "../loja/loja.html"
->>>>>>> origin/dev
     } else {
         document.getElementById("error-modal").style.display = "block";
     }
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/dev
->>>>>>> dev
 // 2 Lembrete - Função para fechar o modal de erro
 function fecharError() {
     document.getElementById("error-modal").style.display = "none";
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/dev
->>>>>>> dev
 // 3 Lembrete - Função para mostrar ou ocultar a senha
 function showPassword() {
     var senhaInput = document.getElementById("senha");
@@ -45,11 +22,3 @@ function showPassword() {
         senhaInput.type = "password";
     }
 }
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/dev
->>>>>>> dev
